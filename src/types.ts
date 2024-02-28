0dd4a5dbb483eb60bd956f7527d7773bdbfacbc7
Angelo Soares <angeelosoares@gmail.com>
2024-02-27 21:57:16 -0300

    style: modified eslint rule

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,4 @@
 import csvParser from "csv-parser";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Callback<T = any> = (data?: T) => Promise<void>;
 export type Options = csvParser.Options | string[];
